217f42218e6ffd19efb9f5ffb178c5265a71b26d
prashant.jha2 <prashant.jha@zaggle.in>
2021-06-14 18:26:41 +0530

    Switch condition

---
+++ Switch.js
@@ -0,0 +1,14 @@
+function Switch(value){
+    switch(value){
+        case 'Ram':
+            console.log('It is Ram.');
+            break;
+        case 'Sam':
+            console.log('it is Devil.');
+            break;
+        default:
+            console.log('Empty');
+    }
+
+}
+Switch('Ram')
