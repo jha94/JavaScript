5521910a15321d7a8e37da33a2ab7212c0890be0
prashant.jha2 <prashant.jha@zaggle.in>
2021-06-14 18:09:55 +0530

    fundamental string operations

---
+++ String.js
@@ -0,0 +1,8 @@
+let string = "A quick brown fox jumps upon over the lazy dog.";
+console.log(string.includes('fox'));
+console.log(string.indexOf('fox'));
+console.log(string.split(" "));
+console.log(string.concat('hi'));
+console.log(string.length);
+console.log(string.charAt(4));
+console.log(string.replace('fox','deer'));
