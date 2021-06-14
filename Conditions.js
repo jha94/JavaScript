750ec9458b43269641d3f9d35e411fcc2bcdb701
prashant.jha2 <prashant.jha@zaggle.in>
2021-06-14 17:55:20 +0530

    Conditions.js

---
+++ Conditions.js
@@ -0,0 +1,12 @@
+function Conditions(){
+    let a = 1;
+    let b = 2;
+    if(a>b){
+        console.log('A > B');
+    } else if(a==b){
+        console.log('A==b')
+    } else{
+        console.log('B > A')
+    }
+}
+Conditions();
